396b1bf97cc694c43c43127778f9a9489a076cba
varshin5699 <136289580+varshin5699@users.noreply.github.com>
2024-07-10 16:02:42 +0530

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 import transformers
 from trl import SFTTrainer
 from dataloader import Tokenizer_preprocess
-from prompts import generate_completion
+from prompt import generate_completion
 import argparse
 
 def print_trainable_parameters(model):
