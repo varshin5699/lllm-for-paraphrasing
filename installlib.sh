d587e802e778ad4518b9f42b30801195250ec58f
varshin5699 <136289580+varshin5699@users.noreply.github.com>
2024-07-10 15:14:56 +0530

    Update installlib.sh

--- installlib.sh
+++ installlib.sh
@@ -3,7 +3,7 @@ pip install -q -U bitsandbytes
 pip install -q -U git+https://github.com/huggingface/transformers.git
 pip install -q -U git+https://github.com/huggingface/peft.git
 pip install -q -U git+https://github.com/huggingface/accelerate.git
-
+pip install -q trl
 #chmod +x installlib.sh to change dependencies
 #to run in a colab environment use
 #!bash install_dependencies.sh
