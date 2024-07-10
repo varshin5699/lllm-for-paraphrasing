c123f965242e8a5f19dfd8bcaf7eaaad68adab72
varshin5699 <136289580+varshin5699@users.noreply.github.com>
2024-07-10 16:11:04 +0530

    Update test.py

--- test.py
+++ test.py
@@ -24,7 +24,7 @@ def print_trainable_parameters(model):
     )
 
 ref2 = args.ref2
-model_id = 
+model_id = "google-t5/t5-small"
 bnb_config = BitsAndBytesConfig(
     load_in_4bit=True,
     bnb_4bit_use_double_quant=True,
